7501c3cf656fe466765c3bbfb712c6b4befb5af8
Prisca Amaechi <102432671+Prissy-Ama@users.noreply.github.com>
2022-06-19 10:56:24 -0500

    updated
    I used the keyword createsuperuser to create admin access to Django.

---
+++ prisca_amaechi/prisca_amaechi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for prisca_amaechi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prisca_amaechi.settings')
+
+application = get_asgi_application()
